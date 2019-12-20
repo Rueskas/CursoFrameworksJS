cd04a8dda3bb61d25c450aaf881e8b0094c63acd
Sergio Ruescas <Rueskas@hotmail.com>
2019-12-20 16:06:13 +0100

    Directivas - Clase 111

--- AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -6,14 +6,18 @@ import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
   styleUrls: ['./peliculas.component.css']
 })
 export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
-  ngOnDestroy() {
-    console.log("El componente se va a eliminar");
-  }
+  public peliculas: any[];
 
   public titulo: String;
   constructor() { 
     console.log("Contructor Lanzado");
     this.titulo = "Componente peliculas";
+    this.peliculas = [
+      {year: 2002, title: "Spiderman 4", image: "https://www.infobae.com/new-resizer/XkfkqIeQ16c2-lbWKdS7OFcbWoY=/750x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/11/16142656/Spiderman.jpg"},
+      {year: 2019, title: "Los vengadores", image: "https://i.blogs.es/876f04/los-vengadores-4/450_1000.jpg" },
+      {year: 2019, title: "El risas", image: "https://i.blogs.es/d0ea1e/joker-joaquin-phoenix/450_1000.jpg"},
+      {year: 2014, title: "Harry Potter y el caliz de fuego", image: "https://i.ytimg.com/vi/CKt0PQFECfs/maxresdefault.jpg"}
+    ]
   }
 
   ngOnInit() {
@@ -22,6 +26,10 @@ export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
 
   ngDoCheck() {
     console.log("DoCheck lanzado");
+  }
+
+  ngOnDestroy() {
+    console.log("El componente se va a eliminar");
   }
 
   cambiarTitulo(){
