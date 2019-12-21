fb58631fcfa977ccad377755ae905f15e3dd6161
Sergio Ruescas <Rueskas@hotmail.com>
2019-12-21 16:48:02 +0100

    Pipes - Clase 119

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { FormularioComponent } from './components/formulario/formulario.componen
 import { PaginaComponent } from './components/pagina/pagina.component';
 import { ErrorComponent } from './components/error/error.component';
 import { PeliculaComponent } from './components/pelicula/pelicula.component';
-
+import {EsParPipe} from './pipes/espar.pipe';
 
 @NgModule({
   declarations: [
@@ -33,7 +33,8 @@ import { PeliculaComponent } from './components/pelicula/pelicula.component';
     FormularioComponent,
     PaginaComponent,
     ErrorComponent,
-    PeliculaComponent
+    PeliculaComponent,
+    EsParPipe
   ],
   imports: [
     BrowserModule,
---
+++ AprendiendoAngular/src/app/pipes/espar.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'espar'
+})
+
+export class EsParPipe implements PipeTransform{
+    transform(value: any){
+        var espar = "no es par";
+        if(value % 2 == 0){
+            espar = "es par";
+        }
+        return "El año es: " + value + " y " + espar;
+    }
+}
